9d6c8790958058e035825c8cc9ad39e6144b9956
Éric Lemoine <eric.lemoine@gmail.com>
2013-05-16 16:13:14 +0200

    Angularize application

--- geosearch.js
+++ geosearch.js
@@ -1,39 +1,55 @@
-$('#geosearch').typeahead([{
-  name: 'geonames',
-  header: '<strong>geonames:</strong>',
-  valueKey: 'name',
-  template: function(context) {
-    return '<div>' + context.name + '<small class="pull-right">(' + context.countryName + ')</small></div>';
-  },
-  remote: {
-    url: 'http://api.geonames.org/searchJSON?username=fredj&q=%QUERY',
-    filter: function(response) {
-      return response.geonames;
+app.directive('olSearch', ['$parse', function($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+
+     var map = $parse(attrs.olSearchMap)(scope);
+
+     element.typeahead([{
+       name: 'geonames',
+       header: '<strong>geonames:</strong>',
+       valueKey: 'name',
+       template: function(context) {
+         return '<div>' + context.name + '<small class="pull-right">(' +
+             context.countryName + ')</small></div>';
+       },
+       remote: {
+         url: 'http://api.geonames.org/searchJSON?username=fredj&q=%QUERY',
+         filter: function(response) {
+           return response.geonames;
+         }
+       }
+     }, {
+       name: 'swisssearch',
+       header: '<strong>swisssearch:</strong>',
+       valueKey: 'label',
+       template: function(context) {
+         return '<div>' + context.name + '<small class="pull-right">(' +
+             context.service + ')</small></div>';
+       },
+       remote: {
+         url: 'http://api.geo.admin.ch/main/wsgi/swisssearch/geocoding?query=%QUERY',
+         filter: function(response) {
+           return $.map(response.results, function(val) {
+             val.label = val.label.replace('<b>', '').replace('</b>', '');
+             return val;
+           });
+         }
+       }
+     }]).on('typeahead:selected', function(event, datum) {
+       // FIXME: the selected lon/lat should be placed on the scope,
+       // and data binding should be used to change the map view when
+       // lon and lat change on the scope.
+       if (datum.lng && datum.lat) {
+         // from geonames
+         var center = ol.projection.transform([datum.lng, datum.lat], 'EPSG:4326', 'EPSG:3857');
+         map.getView().setCenter(center);
+       } else {
+         // from swisssearch
+         //FIXME
+       }
+     });
+
     }
-  }
-}, {
-  name: 'swisssearch',
-  header: '<strong>swisssearch:</strong>',
-  valueKey: 'label',
-  template: function(context) {
-    return '<div>' + context.name + '<small class="pull-right">(' + context.service + ')</small></div>';
-  },
-  remote: {
-    url: 'http://api.geo.admin.ch/main/wsgi/swisssearch/geocoding?query=%QUERY',
-    filter: function(response) {
-      return $.map(response.results, function(val) {
-        val.label = val.label.replace('<b>', '').replace('</b>', '');
-        return val;
-      });
-    }
-  }
-}]).on('typeahead:selected', function(event, datum) {
-  if (datum.lng && datum.lat) {
-    // from geonames
-    var center = ol.projection.transform([datum.lng, datum.lat], 'EPSG:4326', 'EPSG:3857');
-    map.getView().setCenter(center);
-  } else {
-    // from swisssearch
-    //FIXME
-  }
-});
+  };
+}]);
--- map.js
+++ map.js
@@ -1,30 +1,9 @@
-var map = new ol.Map({
-  controls: ol.control.defaults({
-    attributionOptions: {
-      // FIXME: https://github.com/openlayers/ol3/issues/682
-      target: document.getElementById('ol-attribution')
-    }
-  }, [
-    // new ol.control.ScaleLine({
-    //   target: document.getElementById('ol-scale-line'),
-    //   units: ol.control.ScaleLineUnits.METRIC
-    // })
-  ]),
-  layers: [
-    new ol.layer.TileLayer({
-      source: new ol.source.Stamen({
-        layer: 'watercolor'
-      })
-    }),
-    new ol.layer.TileLayer({
-      source: new ol.source.Stamen({
-        layer: 'terrain-labels'
-      })
-    })
-  ],
-  target: 'map',
-  view: new ol.View2D({
-    center: [0, 0],
-    zoom: 2
-  })
-});
+app.directive('olMap', ['$parse', function($parse) {
+ return {
+   restrict: 'A',
+   link: function(scope, element, attrs) {
+     var map = $parse(attrs.olMap)(scope);
+     map.setTarget(element[0]);
+   }
+ };
+}]);
